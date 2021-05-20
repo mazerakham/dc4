b9a5ea90de778af57030f243382ec684e304620e
Jake Mirra <jpluto909@gmail.com>
2021-05-19 22:36:12 -0500

    Debugging an API URL issue.

--- src/dc4/DC4Controller.java
+++ src/dc4/DC4Controller.java
@@ -1,17 +1,26 @@
 package dc4;
 
 import bowser.model.Controller;
+import bowser.model.Handler;
 import bowser.template.Data;
+import ox.Log;
 
 public class DC4Controller extends Controller {
 
   @Override
   public void init() {
     route("GET", "/").to("home.html").data(data);
+    route("GET", "/hello").to(helloHandler);
   }
 
   private final Data data = context -> {
     context.put("apiUrl", DC4Server.API_URL);
+  };
+
+  private final Handler helloHandler = (request, response) -> {
+    String msg = "This hit the webserver and it needed to hit the API server.";
+    Log.debug(msg);
+    response.write(msg);
   };
 
 }
