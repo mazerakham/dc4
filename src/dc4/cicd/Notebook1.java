00438e9d143f6d0dbf8046c4872c3608383e8407
Jake Mirra <jpluto909@gmail.com>
2021-07-11 13:00:34 -0500

    Getting started with a cicd build script.

--- src/dc4/cicd/Notebook1.java
+++ src/dc4/cicd/Notebook1.java
@@ -7,13 +7,17 @@ public class Notebook1 {
 
   public void run() {
     JarJar.project(File.home("workspace/dc4"))
-      .main("dc4.DC4Server")
-      .skipCompile()
-      .clean(false)
-      .verbose()
-      .build(File.downloads("DC4Server.jar"));
-  } 
-  
+        .main("dc4.DC4Server")
+        .skipCompile()
+        .clean(false)
+        .verbose()
+        .build(File.downloads("DC4Server.jar"));
+
+    SSHConnection connection = new SSHConnection("jakemirra.com");
+    
+    connection.shell().run("");
+  }
+
   public static void main(String... args) {
     new Notebook1().run();
   }
